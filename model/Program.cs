749b95aba81c831f81cd48cb451663bb71846942
Christian Alberto Tamayo Robayo <christian.tamayo1@correo.icesi.edu.co>
2019-01-26 13:32:12 -0500

    Cambio 2

--- model/Program.cs
+++ model/Program.cs
@@ -8,7 +8,7 @@ namespace model {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hola");
-
+            Console.WriteLine("Hola");
         }
     }
 }
