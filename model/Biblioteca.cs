16f8928adef6a7838baf12b775410625f358910f
Jehm09 <estephenher999@gmail.com>
2019-01-28 21:48:12 -0500

    .

--- model/Biblioteca.cs
+++ model/Biblioteca.cs
@@ -38,25 +38,32 @@ namespace model
         }
 
         public void cargar(){
-            // Leyendo los archivos
-            TextReader leer = new StreamReader("Libros.txt");
-            
-            string m = leer.ReadLine();
 
-            while(!string.IsNullOrEmpty(m)) {
-                string[] temp = m.Split(',');
-                string nombre = temp[0];
-                int peso = int.Parse(temp[4]);
-                string fecha = temp[3];
-                string autor = temp[1];
-                string edicion = temp[2];
-                // Creación del objeto
-                Libro a = new Libro(nombre, peso, fecha, autor, edicion);
-                libro.Add(a);
-                m = leer.ReadLine();
+            try {
+                // Leyendo los archivos
+                TextReader leer = new StreamReader("Libros.txt");
+            
+                string m = leer.ReadLine();
+
+                while(!string.IsNullOrEmpty(m)) {
+                    string[] temp = m.Split(',');
+                    string nombre = temp[0];
+                    int peso = int.Parse(temp[4]);
+                    string fecha = temp[3];
+                    string autor = temp[1];
+                    string edicion = temp[2];
+                    // Creación del objeto
+                    Libro a = new Libro(nombre, peso, fecha, autor, edicion);
+                    libro.Add(a);
+                    m = leer.ReadLine();
+                }
+                // Cerrar lectura
+                leer.Close();
             }
-            // Cerrar lectura
-            leer.Close();
+            catch (Exception e) {
+            }
+
+
         }
         // Métodos
 
